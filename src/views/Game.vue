036b35fbfa64cd964a6b8aa0a62b91d285280d24
Alyssa <fusaroalyssa@gmail.com>
2018-10-29 13:55:07 -0400

    Real world UI and cors

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -5,39 +5,32 @@
     </div>
 
     <div class="row">
-            <div class="col-md-4">
-                <div class="card" >
-                <img class="card-img-top" src="" alt="Card image cap">
+        <div class="col-md-4">
+            <div class="card" >
                 <div class="card-body">
                     <h5 class="card-title">Players</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
-        </div>
-        <div class="col-md-4">
             <div class="card" >
-                <img class="card-img-top" src="" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title"></h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Flip Picture</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card" >
-                <img class="card-img-top" src="" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">My Captions</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a @click="" class="btn btn-primary">Go somewhere</a>
-                </div>
+                    <ul class="list-group list-group-flush">
+                        <li v-for="c in myCaptions" class="list-group-item">{{c}}</li>
+                    </ul>
+                  </div>
             </div>
         </div>
         <div class="col-md-4">
             <div class="card" >
-                <img class="card-img-top" src="" alt="Card image cap">
+                <img class="card-img" :src="state.picture.url" :alt="state.picture.name">
+                <a @click.prevent="flipPicture" class="btn btn-primary">Flip Picture</a>
+
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card" >
                 <div class="card-body">
                     <h5 class="card-title">Played Captions</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -54,19 +47,24 @@
 </style>
 
 <script>
-import { GetState, FlipPicture } from '@/services/api_access'
+import { GetState, FlipPicture, GetMyCaptions } from '@/services/api_access';
 
 export default {
     data: function(){
         return {
             state: {
-                picture: ""
-            }
+                picture: "",
+                players: [],
+                playedCaptions: [],
+            },
+            myCaptions: [],
         }
     },
     created: function(){
         GetState()
-        .then(x=> this.state = x)
+        .then(x=> this.state = x);
+        GetMyCaptions()
+        .then(x=> this.myCaptions = x);
     },
     methods: {
         flipPicture: function(){
